f309e620b4970e1818c1af43e62a345176a0c2c2
Adyot Akhouri <74779256+adyot2011@users.noreply.github.com>
2022-05-03 13:45:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song = ""
+leftWristX= 0;
+leftWristY= 0;
+RightWristX= 0;
+RightWristY= 0;
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+}
+
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
